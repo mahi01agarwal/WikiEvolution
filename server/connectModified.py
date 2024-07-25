2af9e54577f01195cec05da2f7d7b323736e2ca0
mahi01agarwal <agarwalmahima01@gmail.com>
2024-07-25 18:50:59 +0530

    Add database logic

---
+++ server/connectModified.py
@@ -0,0 +1,81 @@
+# connect.py
+
+import pymysql
+from sshtunnel import SSHTunnelForwarder
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+
+# Database configuration from environment variables
+SSH_HOST = os.getenv('SSH_HOST')
+SSH_USER = os.getenv('SSH_USER')
+SSH_KEY_FILE = os.getenv('SSH_KEY_FILE')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = int(os.getenv('MYSQL_PORT'))
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+def connect_to_db_via_ssh():
+    try:
+        tunnel = SSHTunnelForwarder(
+            (SSH_HOST, 22),
+            ssh_username=SSH_USER,
+            ssh_private_key=SSH_KEY_FILE,
+            remote_bind_address=(MYSQL_HOST, MYSQL_PORT)
+        )
+        tunnel.start()
+        print("SSH Tunnel established")
+        conn = pymysql.connect(
+            host='127.0.0.1',
+            port=tunnel.local_bind_port,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE,
+            connect_timeout=30,
+            read_timeout=30,
+            autocommit=True  # Enable autocommit
+        )
+        print("Connection successful!")
+        return conn, tunnel
+    except Exception as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None, None
+
+def execute_query(query, fetch=True):
+    conn, tunnel = connect_to_db_via_ssh()
+    if conn and tunnel:
+        try:
+            with conn.cursor() as cursor:
+                print("Executing query...")
+                cursor.execute(query)
+                if fetch:
+                    result = cursor.fetchall()
+                    print("Query executed successfully")
+                    return result
+                else:
+                    conn.commit()
+                    print("Query executed and changes committed")
+                    return None
+        except pymysql.MySQLError as e:
+            print(f"MySQL error during query execution: {e}")
+        except Exception as e:
+            print(f"General error during query execution: {e}")
+        finally:
+            conn.close()
+            tunnel.stop()
+            print("MySQL connection and SSH tunnel are closed")
+    else:
+        print("Failed to connect to MySQL using SSH tunnel.")
+        return None
+
+def check_table_exists(table_name):
+    query = f"SHOW TABLES LIKE '{table_name}';"
+    result = execute_query(query)
+    return len(result) > 0
+
+def create_tables(wikiproject_name):
+    print("for now pass")
+    pass
